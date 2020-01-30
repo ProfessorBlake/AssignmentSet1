ad490eca80d248500aced194fd7170c930176b21
Blake <telefragent@gmail.com>
2020-01-30 17:47:43 -0500

    Car

---
+++ Assets/Exercise1.2/Scripts/Car.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+	public GameObject WallRight;
+	public GameObject WallLeft;
+	public float MoveSpeed;
+
+	private void Update()
+	{
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += new Vector3(MoveSpeed, 0f, 0f);
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += new Vector3(-MoveSpeed, 0f, 0f);
+		}
+
+		if(transform.position.x < 0f)
+		{
+			WallLeft.SetActive(false);
+		}
+		else
+		{
+			WallLeft.SetActive(true);
+		}
+
+		if (transform.position.x > 0f)
+		{
+			WallRight.SetActive(false);
+		}
+		else
+		{
+			WallRight.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Exercise1.2/Scripts/WallController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour
+{
+	public bool On;
+
+    public void ToggleWall()
+	{
+		On = !o
+		Debug.Log(gameObject.name + " " + On);
+	}
+}
